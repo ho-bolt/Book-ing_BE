09c986a609b72d6e3ef38ec4715be5768e61fbe6
Hojin <gbolt@naver.com>
2022-05-10 00:10:20 +0900

    ff

--- public/js/app.js
+++ public/js/app.js
@@ -212,11 +212,11 @@ function makeConnection() {
         iceServers: [
             {
                 urls: [
-                    { url: 'stun:stun.l.google.com:19302' },
-                    { url: 'stun:stun1.l.google.com:19302' },
-                    { url: 'stun:stun2.l.google.com:19302' },
-                    { url: 'stun:stun3.l.google.com:19302' },
-                    { url: 'stun:stun4.l.google.com:19302' },
+                    'stun:stun.l.google.com:19302',
+                    'stun:stun1.l.google.com:19302',
+                    'stun:stun2.l.google.com:19302',
+                    'stun:stun3.l.google.com:19302',
+                    'stun:stun4.l.google.com:19302',
                 ],
             },
         ],
