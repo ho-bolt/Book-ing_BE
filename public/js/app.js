7e5e681d373171b5dfa78711486b28c4c1890b24
Hojin <gbolt@naver.com>
2022-05-10 19:23:14 +0900

    dd

--- public/js/app.js
+++ public/js/app.js
@@ -214,23 +214,9 @@ function makeConnection() {
         {
             iceServers: [
                 {
-                    urls: [
-                        "stun01.sipphone.com",
-                        "stun.ekiga.net",
-                        "stun.fwdnet.net",
-                        "stun.ideasip.com",
-                        "stun.iptel.org",
-                        "stun.rixtelecom.se",
-                        "stun.schlund.de",
-                        "stunserver.org",
-                        "stun.softjoys.com",
-                        "stun.voiparound.com",
-                        "stun.voipbuster.com",
-                        "stun.voipstunt.com",
-                        "stun.voxgratia.org",
-                        "stun.xten.com",
-
-                    ]
+                    urls: "turn:TURN_IP:3478",
+                    username: "test",
+                    credential: "pass",
                 },
             ],
         }
