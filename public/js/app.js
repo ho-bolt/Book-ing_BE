e16399f866622629d962ba3158aade363d3f5ab3
Hojin <gbolt@naver.com>
2022-05-10 17:40:41 +0900

    턴 서버

--- public/js/app.js
+++ public/js/app.js
@@ -208,23 +208,10 @@ function makeConnection() {
     myPeerConnection = new RTCPeerConnection({
         iceServers: [
             {
-                urls: "stun:openrelay.metered.ca:80",
-            },
-            {
-                urls: "turn:openrelay.metered.ca:80",
-                username: "openrelayproject",
-                credential: "openrelayproject",
-            },
-            {
-                urls: "turn:openrelay.metered.ca:443",
-                username: "openrelayproject",
-                credential: "openrelayproject",
-            },
-            {
-                urls: "turn:openrelay.metered.ca:443?transport=tcp",
-                username: "openrelayproject",
-                credential: "openrelayproject",
-            },
+                urls: "turn:TURN_IP:3478",
+                username: "test",
+                password: "test1234"
+            }
         ],
     });
     //answer와 offer 서로 교환 끝나면 이거 필요
