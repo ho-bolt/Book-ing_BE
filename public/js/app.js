1db649abed48786286ad86f3654f6bbb30bae924
Hojin <gbolt@naver.com>
2022-05-10 22:48:10 +0900

    dd

--- public/js/app.js
+++ public/js/app.js
@@ -213,11 +213,24 @@ function makeConnection() {
     myPeerConnection = new RTCPeerConnection(
         {
             iceServers: [
+                {
+                    urls: "stun:stun.l.google.com:19302",
+                },
+                {
+                    urls: "turn:192.158.29.39:3478?transport=udp",
+                    credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+                    username: "28224511:1379330808",
+                },
+                {
+                    urls: "turn:192.158.29.39:3478?transport=tcp",
+                    credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+                    username: "28224511:1379330808",
+                },
 
                 {
-                    urls: "turn:TURN_IP:3478",
+                    urls: "turn:13.125.123.252:3478",
                     username: "test",
-                    credential: "pass",
+                    credential: "test123",
                 }
             ],
         }
