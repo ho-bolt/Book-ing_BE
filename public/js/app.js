d01ee3e77f06d16d397980eb5d445a83816f9fa8
Hojin <gbolt@naver.com>
2022-05-11 15:55:05 +0900

    aa

--- public/js/app.js
+++ public/js/app.js
@@ -185,27 +185,28 @@ function makeConnection() {
     myPeerConnection = new RTCPeerConnection(
         {
             iceServers: [
-                {
-                    urls: [
-                        "stun:stun.l.google.com:19302",
-                        "stun:stun1.l.google.com:19302",
-                        "stun:stun2.l.google.com:19302",
-                        "stun:stun3.l.google.com:19302",
-                        "stun:stun4.l.google.com:19302",
-                    ]
-                },
+                { url: 'stun:stun.l.google.com:19302' },
+                { url: 'stun:stun1.l.google.com:19302' },
+                { url: 'stun:stun2.l.google.com:19302' },
+                { url: 'stun:stun3.l.google.com:19302' },
+                { url: 'stun:stun4.l.google.com:19302' },
+
 
                 {
-                    url: 'turn:relay.backups.cz',
-                    credential: 'webrtc',
-                    username: 'webrtc'
+                    url: 'turn:numb.viagenie.ca',
+                    credential: 'muazkh',
+                    username: 'webrtc@live.com'
                 },
                 {
-                    url: 'turn:relay.backups.cz?transport=tcp',
-                    credential: 'webrtc',
-                    username: 'webrtc'
+                    url: 'turn:192.158.29.39:3478?transport=udp',
+                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                    username: '28224511:1379330808'
                 },
-
+                {
+                    url: 'turn:192.158.29.39:3478?transport=tcp',
+                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                    username: '28224511:1379330808'
+                }
             ],
         }
     );
