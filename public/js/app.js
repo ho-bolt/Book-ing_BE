592a385398f0c873d96465fbdde38544de5c59ea
Hojin <gbolt@naver.com>
2022-05-11 16:15:01 +0900

    a

--- public/js/app.js
+++ public/js/app.js
@@ -188,13 +188,9 @@ function makeConnection() {
                 {
                     urls: "stun:openrelay.metered.ca:80",
                 },
+
                 {
                     urls: "turn:openrelay.metered.ca:80",
-                    username: "openrelayproject",
-                    credential: "openrelayproject",
-                },
-                {
-                    urls: "turn:openrelay.metered.ca:443",
                     username: "openrelayproject",
                     credential: "openrelayproject",
                 },
