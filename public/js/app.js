ca6fe4a790d094f39050a5c93a28799dc03b55aa
Hojin <gbolt@naver.com>
2022-05-11 16:38:09 +0900

    g

--- public/js/app.js
+++ public/js/app.js
@@ -185,7 +185,12 @@ function makeConnection() {
         {
             iceServers: [
                 {
-                    urls: "stun:stun.l.google.com:19302",
+                    urls: ["stun:stun.l.google.com:19302",
+                        "stun:stun1.l.google.com:19302",
+                        "stun:stun2.l.google.com:19302",
+                        "stun:stun3.l.google.com:19302",
+                        "stun:stun4.l.google.com:19302",
+                    ]
                 },
                 {
                     url: 'turn:numb.viagenie.ca',
