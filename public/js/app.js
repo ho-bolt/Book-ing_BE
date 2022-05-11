b528fa968b6346149151073aa0dedfdb11748e43
Hojin <gbolt@naver.com>
2022-05-11 16:31:35 +0900

    a

--- public/js/app.js
+++ public/js/app.js
@@ -187,11 +187,7 @@ function makeConnection() {
                 {
                     urls: "stun:stun.l.google.com:19302",
                 },
-                {
-                    url: 'turn:numb.viagenie.ca',
-                    credential: 'muazkh',
-                    username: 'webrtc@live.com'
-                },
+
                 {
                     url: 'turn:192.158.29.39:3478?transport=udp',
                     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
