59d2e9809a08e92e2c7511a87c8810084c0995af
Hojin <gbolt@naver.com>
2022-05-11 17:00:18 +0900

    a

--- public/js/app.js
+++ public/js/app.js
@@ -185,10 +185,7 @@ function makeConnection() {
         {
             iceServers: [
                 {
-                    urls:
-                        "stun:stun.l.google.com:19302",
-
-
+                    urls: "stun:stun.l.google.com:19302",
                 },
                 {
                     url: 'turn:numb.viagenie.ca',
@@ -200,14 +197,6 @@ function makeConnection() {
                     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
                     username: '28224511:1379330808'
                 },
-                {
-                    url: 'turn:192.158.29.39:3478?transport=tcp',
-                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                    username: '28224511:1379330808'
-                },
-
-
-
 
             ],
         }
