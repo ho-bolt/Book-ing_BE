384e51d0660e73e7cae0e39804b2fc4ef7d4460f
Hojin <gbolt@naver.com>
2022-05-11 15:52:21 +0900

    a

--- public/js/app.js
+++ public/js/app.js
@@ -194,15 +194,16 @@ function makeConnection() {
                         "stun:stun4.l.google.com:19302",
                     ]
                 },
+
                 {
-                    url: 'turn:numb.viagenie.ca',
-                    credential: 'muazkh',
-                    username: 'webrtc@live.com'
+                    url: 'turn:relay.backups.cz',
+                    credential: 'webrtc',
+                    username: 'webrtc'
                 },
                 {
-                    url: 'turn:192.158.29.39:3478?transport=udp',
-                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                    username: '28224511:1379330808'
+                    url: 'turn:relay.backups.cz?transport=tcp',
+                    credential: 'webrtc',
+                    username: 'webrtc'
                 },
 
             ],
