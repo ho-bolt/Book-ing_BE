187df25a27a9721ebe434194d17e1b5be7889d17
Hojin <gbolt@naver.com>
2022-05-11 16:18:49 +0900

    a

--- public/js/app.js
+++ public/js/app.js
@@ -186,22 +186,22 @@ function makeConnection() {
         {
             iceServers: [
                 {
-                    urls: "stun:openrelay.metered.ca:80",
+                    urls: "stun:stun.l.google.com: 19302",
                 },
                 {
-                    urls: "turn:openrelay.metered.ca:80",
-                    username: "openrelayproject",
-                    credential: "openrelayproject",
+                    url: 'turn:numb.viagenie.ca',
+                    credential: 'muazkh',
+                    username: 'webrtc@live.com'
                 },
                 {
-                    urls: "turn:openrelay.metered.ca:443",
-                    username: "openrelayproject",
-                    credential: "openrelayproject",
+                    url: 'turn:192.158.29.39:3478?transport=udp',
+                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                    username: '28224511:1379330808'
                 },
                 {
-                    urls: "turn:openrelay.metered.ca:443?transport=tcp",
-                    username: "openrelayproject",
-                    credential: "openrelayproject",
+                    url: 'turn:192.158.29.39:3478?transport=tcp',
+                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                    username: '28224511:1379330808'
                 },
             ],
         }
