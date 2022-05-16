cd19cd6103147df83a503033661b473b7e8f5b2e
Hojin <gbolt@naver.com>
2022-05-16 20:49:58 +0900

    s

--- public/js/app.js
+++ public/js/app.js
@@ -270,9 +270,7 @@ function handleAddStream(data, remoteSocketId) {
 
     console.log("@@@@@", peerStream)
     if (data.track.kind === 'video') {
-        if (!collectiSoketId.includes(remoteSocketId)) {
-            paintPeerFace(peerStream, remoteSocketId)
-        }
+        paintPeerFace(peerStream, remoteSocketId)
     }
     // const peersFace = document.getElementById('peersFace');
     // peersFace.srcObject = data.stream;
