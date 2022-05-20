b3a2d3986757f19a018a9d5c5b27a4cc64af89bd
Hojin <gbolt@naver.com>
2022-05-20 12:36:11 +0900

    g

--- public/js/app.js
+++ public/js/app.js
@@ -409,7 +409,8 @@ async function shareScreen() {
     console.log("AAa", myPeerConnection)
     navigator.mediaDevices.getDisplayMedia({ cursor: true }).then(stream => {
         const screenTrack = stream.getTracks()[0];
-        console.log("컴퓨터객체", pcObj.socket.id)
+        console.log("피어들", pcObj)
+        console.log("컴퓨터객체", pcObj.socketId)
         pcObj.find(sender => sender.track.kind === 'video').replaceTrack(screenTrack);
         screenTrack.onended = function () {
             pcObj.find(sender => sender.track.kind === 'video').replaceTrack(userStream.current.getTracks()[1]);
