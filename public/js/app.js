8a882ff52bd8235e15f8cd156b399d32c6410563
Hojin <gbolt@naver.com>
2022-05-26 22:30:37 +0900

    g

--- public/js/app.js
+++ public/js/app.js
@@ -398,25 +398,39 @@ function handleAddStream(data, remoteSocketId) {
     // peersFace.srcObject = data.stream;
 }
 
-async function shareScreen() {
-    let displayMediaStream = await navigator.mediaDevices.getDisplayMedia({ audio: true, video: true });
-    console.log("화면", displayMediaStream)
+// async function shareScreen() {
+//     let displayMediaStream = await navigator.mediaDevices.getDisplayMedia({ audio: true, video: true });
+//     console.log("화면", displayMediaStream)
 
-    console.log("화면1", displayMediaStream.getTracks()[0])
-    const screenTrack = displayMediaStream.getTracks()[0];
-    console.log("screenTrack", screenTrack)
-    console.log("@@@@@@@", senders)
-    console.log("@@@@@@@track", senders[1].track)
-    senders.find(sender => sender.track.kind === 'video').replaceTrack(displayMediaStream.getTracks()[0]);
-    screenTrack.onended = function () {
-        senders.find(sender => sender.track.kind === 'video').replaceTrack(myStream.getTracks()[1]);
-    };
+//     console.log("화면1", displayMediaStream.getTracks()[0])
+//     const screenTrack = displayMediaStream.getTracks()[0];
+//     console.log("screenTrack", screenTrack)
+//     console.log("@@@@@@@", senders)
+//     console.log("@@@@@@@track", senders[1].track.kind)
+//     console.log("야호", myPeerConnection.getSenders())
 
-    // paintScreen(displayMediaStream)
-    document.getElementById('screenShare').srcObject = screenTrack;
+//     senders.find(sender => sender.track.kind === 'video').replaceTrack(screenTrack);
+//     screenTrack.onended = function () {
+//         senders.find(sender => sender.track.kind === 'video').replaceTrack(myStream.getTracks()[1]);
+//     };
 
+//     // paintScreen(displayMediaStream)
+//     document.getElementById('screenShare').srcObject = displayMediaStream;
+
+//}
+
+function shareScreen() {
+    navigator.mediaDevices.getDisplayMedia({ cursor: true }).then(stream => {
+
+        const screenTrack = stream.getTracks()[0];
+        console.log('화면', screenTrack)
+        console.log('222', senders[1].track.kind)
+        senders.find(sender => sender.track.kind === 'video').replaceTrack(screenTrack);
+        screenTrack.onended = function () {
+            senders.find(sender => sender.track.kind === "video").replaceTrack(myStream.getTracks()[1]);
+        }
+    })
 }
-
 
 
 function paintScreen(screen) {
