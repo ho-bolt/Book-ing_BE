59491f74ecddb084bd80958aa07c2bc6d5ce8f45
Hojin <gbolt@naver.com>
2022-05-26 19:51:53 +0900

    g

--- public/js/app.js
+++ public/js/app.js
@@ -454,12 +454,13 @@ function deleteVideo(leavedSocketId) {
 
 async function shareScreen() {
     let displayMediaStream = await navigator.mediaDevices.getDisplayMedia();
+    console.log("화면", displayMediaStream)
     console.log("@@@@@@@", senders)
     console.log("@@@@@@@track", senders[1].track)
     senders.find(sender => sender.track.kind === 'video').replaceTrack(displayMediaStream.getTracks()[0]);
-    screenTrack.onended = function () {
-        senders.find(sender => sender.track.kind === 'video').replaceTrack(myStream.getTracks()[1]);
-    };
+    // screenTrack.onended = function () {
+    //     senders.find(sender => sender.track.kind === 'video').replaceTrack(myStream.getTracks()[1]);
+    // };
     document.getElementById('screenShare').srcObject = displayMediaStream;
 }
 
