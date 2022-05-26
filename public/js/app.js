dbcf2afdb0b1179b4df1dcb21db0702f200d7037
Hojin <gbolt@naver.com>
2022-05-26 20:06:14 +0900

    g

--- public/js/app.js
+++ public/js/app.js
@@ -348,6 +348,7 @@ function makeConnection(remoteSocketId) {
     // console.log('첨 들어온 사람 피어', myPeerConnection);
 
 
+    console.log("내 피어 커넥션", myPeerConnection.ontrack)
     myPeerConnection.ontrack = handleTrackEvent;
     myPeerConnection.addEventListener('icecandidate', (event) => {
         // console.log("아이스 캔디에이트", event)
